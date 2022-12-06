21804670c76a32ae08093a9d6277afe1bcc42fd1
Toni Ilic <toniilic01@gmail.com>
2022-12-07 00:05:56 +0100

    define main function

--- main.py
+++ main.py
@@ -1,39 +1,44 @@
-roman_dict = {
-    'I' : 1,
-    'V' : 5,
-    'X' : 10,
-    'L' : 50,
-    'C' : 100,
-    'D' : 500,
-    'M' : 1000
-}
+def main():
 
-roman_dict_doubles = {
-    'IV' : 4,
-    'IX' : 9,
-    'XL' : 40,
-    'XC' : 90,
-    'CD' : 400,
-    'CM' : 900
-}
+    roman_dict = {
+        'I' : 1,
+        'V' : 5,
+        'X' : 10,
+        'L' : 50,
+        'C' : 100,
+        'D' : 500,
+        'M' : 1000
+    }
 
-enter = input('Input a roman number: ').upper()
+    roman_dict_doubles = {
+        'IV' : 4,
+        'IX' : 9,
+        'XL' : 40,
+        'XC' : 90,
+        'CD' : 400,
+        'CM' : 900
+    }
 
-final = 0
-adds = 0
-i = len(enter)
+    enter = input('Input a roman number: ').upper()
 
-while i > 0:
-
-    if enter[:2] not in roman_dict_doubles:
-        adds = adds + roman_dict[enter[:1]]
-        replacement = 1
-    else:
-        final = final + roman_dict_doubles[enter[:2]]
-        replacement = 2
-
-    enter = enter[replacement::]
+    final = 0
+    adds = 0
     i = len(enter)
 
+    while i > 0:
 
-print(final+adds)
+        if enter[:2] not in roman_dict_doubles:
+            adds = adds + roman_dict[enter[:1]]
+            replacement = 1
+        else:
+            final = final + roman_dict_doubles[enter[:2]]
+            replacement = 2
+
+        enter = enter[replacement::]
+        i = len(enter)
+
+
+    print(final+adds)
+
+if __name__ == '__main__':
+    main()
